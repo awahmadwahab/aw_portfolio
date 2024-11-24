289e1e92786feefdaf87ee8c270b05f311971d6a
Ahmad Wahab <aw865386@gmail.com>
2024-11-24 23:04:26 +0500

    {commit_message}

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('#typed', {
+        strings: ["Photographer","FrontEnd Developer","Graphic Designer","Content Creator","AW  X  VISION"],
+        typeSpeed: 75,
+        startDelay: 500,
+        backDelay: 1000,
+        backSpeed: 40,
+        loop: true,
+        
+    });
+});
