6a0894be2068d37a0d94315df6d6371fa16f6549
Ahmad Wahab <aw865386@gmail.com>
2024-11-25 16:11:56 +0500

    about secton

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed('#typed', {
         strings: ["Photographer","FrontEnd Developer","Graphic Designer","Content Creator","AW  X  VISION"],
-        typeSpeed: 75,
+        typeSpeed: 50,
         startDelay: 500,
-        backDelay: 1000,
+        backDelay: 800,
         backSpeed: 40,
         loop: true,
         
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
