a54b8f0ac7e55b8616ded51928a135e2898cb4d4
Ahmad Wahab <aw865386@gmail.com>
2024-11-29 16:32:19 +0500

    mobile

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed('#typed', {
-        strings: ["Photographer","FrontEnd Developer","Graphic Designer","Content Creator","AW  X  VISION"],
-        typeSpeed: 50,
-        startDelay: 500,
-        backDelay: 800,
-        backSpeed: 40,
-        loop: true,
-        
+document.addEventListener("DOMContentLoaded", function()
+{
+  var typed = new Typed('#typed', {
+    strings: ["Photographer", "FrontEnd Developer", "Graphic Designer", "Content Creator", "AW  X  VISION"],
+    typeSpeed: 50,
+    startDelay: 500,
+    backDelay: 800,
+    backSpeed: 40,
+    loop: true,
+  });
+
+
+  const navToggle = document.getElementById('nav-toggle');
+  const navList = document.getElementById('nav_list');
+  const navLinks = document.querySelectorAll('#nav_list a');
+
+  navToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navList.classList.remove('active');
     });
+  });
 });
+
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
@@ -18,4 +34,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+
 
